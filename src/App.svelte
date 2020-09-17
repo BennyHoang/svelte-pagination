4ffe94760296bb244174d5cd446bc18a54bcefa4
Benny Hoang <benny.hoang@accenture.com>
2020-09-17 17:34:44 +0200

    fetching data from API

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	export let name;
+	 async function getBitcoinData() {
+
+        let response = await fetch(`https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=100&api_key=8ae55d463e1bf8d38b4a502ca47512f9b1dec21533ad9af7acb993e8ba952bc`);
+		let data = await response.text()
+		console.log("data: ", JSON.parse(data));
+        return data;
+    }
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<button on:click={getBitcoinData}>Click me</button>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
